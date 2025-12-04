7ddb2b768161c5ff927b08c2f391ff07c4f53914
Alexsandro <alexsandro.junior@mirante.net.br>
2025-12-03 22:55:36 -0300

    refactor: logo do header agora retorna para página inicial

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink, useLocation, Link } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import logoTeste from "/logo-teste.png";
 import { useAuth } from "../hooks/useAuth";
@@ -26,7 +26,9 @@ const Header = () => {
     <header className="bg-[linear-gradient(to_right,_#9A238B_51%,_#340C2F_100%)] text-white w-full px-4 py-3">
       <div className="flex justify-between items-center max-w-5xl mx-auto px-2">
         <div className="flex items-center gap-4">
-          <img src={logoTeste} alt="CEUB Logo" className="h-12 w-auto" />
+          <Link to="/home" className="hover:opacity-80 transition">
+            <img src={logoTeste} alt="CEUB Logo" className="h-12 w-auto" />
+          </Link>
           <h1 className="text-white text-xl md:text-2xl font-semibold">
             Núcleo de Esportes
           </h1>
